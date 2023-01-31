179cfe40a7da0eeef4b959d6464af7ac92053e1e
Andriamanitra <mikko77792@gmail.com>
2023-01-31 05:45:51 +0200

    one branch per item type

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,28 +9,32 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-
-            if item.name in ("Aged Brie", "Backstage passes to a TAFKAL80ETC concert"):
+            elif item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11 and item.quality < 50:
-                            item.quality += 1
-                        if item.sell_in < 6 and item.quality < 50:
-                            item.quality += 1
-            elif item.quality > 0:
-                item.quality -= 1
+                item.sell_in -= 1
 
-            item.sell_in -= 1
-
-            if item.sell_in < 0:
-                if item.name == "Aged Brie":
+                if item.sell_in < 0:
                     if item.quality < 50:
                         item.quality += 1
-                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality += 1
+                    if item.sell_in < 11 and item.quality < 50:
+                        item.quality += 1
+                    if item.sell_in < 6 and item.quality < 50:
+                        item.quality += 1
+                item.sell_in -= 1
+
+                if item.sell_in < 0:
                     item.quality = 0
-                elif item.quality > 0:
+            else:
+                if item.quality > 0:
                     item.quality -= 1
+                item.sell_in -= 1
+                if item.sell_in < 0:
+                    if item.quality > 0:
+                        item.quality -= 1
 
 
 class Item:
